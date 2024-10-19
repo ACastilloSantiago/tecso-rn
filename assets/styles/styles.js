8f915fedb97fcbbca10be0ca2f572bcd175649dd
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-10-19 16:52:44 -0300

    button-component: agrego estilos generales

---
+++ assets/styles/styles.js
@@ -0,0 +1,22 @@
+export const colors = {
+  // Brand
+  brandPrimary01: "rgb(240, 131, 24)",
+  brandPrimary02: "rgba(248, 173, 52, 1)",
+  brandPrimary03: "rgba(243, 233, 236, 1)",
+  brandSecondary01: "rgba(1, 153, 163, 1)",
+  brandSecondary02: "rgba(1, 113, 121, 1)",
+  brandSecondary03: "rgba(153, 219, 214, 1)",
+  brandNeutro01: "rgba(95, 91, 91, 1)",
+  brandNeutro02: "rgba(177, 177, 177, 1)",
+  brandNeutro03: "rgba(153, 219, 214, 1)",
+
+  // Background
+  backgroundDarkmodePrimary: "rgba(51, 51, 51, 1)",
+  backgroundDarkmodeSecondary: "rgba(68, 68, 68, 1)",
+  backgroundDarkmodeTertiary: "rgba(43, 43, 43, 1)",
+  backgroundLightmodePrimary: "rgba(255, 255, 255, 1)",
+  backgroundLightmodeSecondary: "rgba(246, 246, 246, 1)",
+
+  // System
+  systemError: "rgba(225, 25, 0, 1)",
+};
